4a8ebe42694f9ec1f81ea907eb8cb80e977d9c73
Vidushan Kohiladas <Vidushan@gmail.com>
2017-12-07 03:17:05 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,18 +12,10 @@ client.Dispatcher.on(Events.GATEWAY_READY, e => {
 console.log("Connected as: " + client.User.username);
 });
 
- var sayings = ['Hell No', 'Yes.', 'STOP ASKING ME SO MANY QUESTIONS', 'Very doubtful.', 'It is certain.', 'Most likely.', 'My reply is no.', 'Fuck no.', "Why is ZEROevoL so cool?"];
-
-var getRandomWord = function () {
-      return sayings[Math.floor(Math.random() * sayings.length)];
-  };
-
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
   const messages = e.message.content
   if (messages == "ping")
   messages.sendMessage("pong");
   if (messages == "1")
   messages.sendMessage("2");
-  if (messages == "!8ball")
-  messages.sendMessage(getRandomWord);
 });
