1c297ee83efbacc5bd6b5e61b5047d1434755464
Vidushan <kvidushan@hotmail.com>
2017-12-07 11:43:24 +0000

    added few more commands

--- bot.js
+++ bot.js
@@ -1,21 +1,38 @@
 // dependencies
-var auth = require('./auth.json');
-var Discordie = require("discordie");
-
-var Events = Discordie.Events;
-var client = new Discordie();
-
-// token stored in a file that is ignored by git
-client.connect({ token: auth.token });
-
-client.Dispatcher.on(Events.GATEWAY_READY, e => {
-console.log("Connected as: " + client.User.username);
+const discord = require('discord.js')
+var token = require('./auth.json');
+const bot = new discord.Client();
+bot.on('ready', () => {
+    console.log('Connected as:', bot.user);
 });
 
-client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-  const messages = e.message.content
-  if (messages == "ping")
-  messages.sendMessage("pong");
-  if (messages == "1")
-  messages.sendMessage("2");
+// Event to listen to messages sent to the server where the bot is located
+bot.on('message', message => {
+    // So the bot doesn't reply to iteself
+    if (message.author.bot) return;
+
+    //reverse command
+    if (message.content.indexOf('!reverse') === 0) {
+      var text = message.content.substring(1);
+      var reversed = '';
+      var i = text.length;
+      while (i > 0) {
+          reversed += text.substring(i - 1, i);
+          i--;
+      }
+      message.reply(reversed);
+    }
+
+    // 8ball command
+    if (message.content.indexOf('!8ball') === 0) {
+      var sayings = ['No', 'Yes.', 'STOP ASKING ME SO MANY QUESTIONS', 'Very doubtful.', 'It is certain.', 'Most likely.', 'My reply is no.'];
+      var rand = sayings[Math.floor(Math.random() * sayings.length)];
+      message.reply(rand)
+    }
+    // ping reply
+    if (message.content.indexOf('ping') === 0) {
+      message.reply('pong');
+    }
+    
 });
+bot.login(token.token);
